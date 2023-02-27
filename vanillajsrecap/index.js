1e8903ebc42afa3302a29f762fe1806e6541d9f1
paullaster <paullasterokoth98@gmail.com>
2023-02-27 21:53:48 +0300

    add filess and changes

--- vanillajsrecap/index.js
+++ vanillajsrecap/index.js
@@ -9,5 +9,8 @@
 // let arrayOfItems = [];
 // console.log(arrayOfItems);
 // 2. Array prototype using new constructor:
-let newArray = new Array(10,2,6,7,8);
-console.log(newArray.length);
+// let newArray = new Array(10,2,6,7,8);
+// console.log(newArray.length);
+
+// Properties of arrays and array methods:
+
